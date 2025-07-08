8790b42d55bc856737da0455a05e0c042dcc0e30
Louis de Looze <louis.delooze@gmail.com>
2025-07-08 15:58:38 +0200

    Check if registration_globlist exists

--- src/ComposerPlugin.php
+++ src/ComposerPlugin.php
@@ -57,46 +57,29 @@ final class ComposerPlugin implements PluginInterface, EventSubscriberInterface
     public function nonComposerRegistration(Event $event): void
     {
         $io = $event->getIO();
-        $io->write('<info>Generating NonComposerComponentRegistration file...</info>');
+        $io->write('<info>NCR: Generating NonComposerComponentRegistration file...</info>');
 
         $basePath = getcwd();
         $fileName = self::NON_COMPOSER_COMPONENT_REGISTRATION;
         $nonComposerComponentFilePath = $basePath . DIRECTORY_SEPARATOR . $fileName;
+
+        if (!file_exists($basePath . DIRECTORY_SEPARATOR . self::REGISTRATION_GLOB_LIST)
+        ) {
+            $io->write('<error>NCR: Skipping generation, missing registration_globlist!</error>');
+            return;
+        }
+
+
         $backupFilePath = $basePath . DIRECTORY_SEPARATOR . self::BACKUP_NON_COMPOSER_COMPONENT_REGISTRATION;
 
         if (file_exists($nonComposerComponentFilePath) && !file_exists($backupFilePath)) {
             rename($nonComposerComponentFilePath, $backupFilePath);
-            $io->write('<comment>Backup of ' . $nonComposerComponentFilePath . ' at ' . $backupFilePath . '</comment>');
+            $io->write('<comment>NCR: Backup of ' . $nonComposerComponentFilePath . ' at ' . $backupFilePath . '</comment>');
         }
 
         $this->dumpRegistration($basePath, $fileName);
 
-        $io->write('<info>Dumped at <comment>`' . $nonComposerComponentFilePath . '`</comment>!</info>');
-    }
-
-    /**
-     * @param string $basePath
-     * @param array $registrationFiles
-     * @return array
-     */
-    public function excludeDisabled(string $basePath, array $registrationFiles): array
-    {
-        if (! file_exists($basePath . DIRECTORY_SEPARATOR . self::NON_COMPOSER_COMPONENT_REGISTRATION_EXCLUDE)) {
-            return $registrationFiles;
-        }
-
-        $excludeList = require $basePath . DIRECTORY_SEPARATOR . self::NON_COMPOSER_COMPONENT_REGISTRATION_EXCLUDE;
-
-        $finalRegistrationFiles = [];
-        foreach ($registrationFiles as $registrationFile) {
-            $path = implode('/', array_slice(explode('/', dirname($registrationFile)), -2));
-            if (\in_array($path, $excludeList, true)) {
-                continue;
-            }
-            $finalRegistrationFiles[] = $registrationFile;
-
-        }
-        return $finalRegistrationFiles;
+        $io->write('<info>NCR: Dumped at <comment>`' . $nonComposerComponentFilePath . '`</comment>!</info>');
     }
 
     private function dumpRegistration(string $basePath, string $fileName): void
@@ -131,5 +114,30 @@ PHP;
 
         file_put_contents($filePath, $phpCode);
 
+    }
+
+    /**
+     * @param string $basePath
+     * @param array $registrationFiles
+     * @return array
+     */
+    private function excludeDisabled(string $basePath, array $registrationFiles): array
+    {
+        if (! file_exists($basePath . DIRECTORY_SEPARATOR . self::NON_COMPOSER_COMPONENT_REGISTRATION_EXCLUDE)) {
+            return $registrationFiles;
+        }
+
+        $excludeList = require $basePath . DIRECTORY_SEPARATOR . self::NON_COMPOSER_COMPONENT_REGISTRATION_EXCLUDE;
+
+        $finalRegistrationFiles = [];
+        foreach ($registrationFiles as $registrationFile) {
+            $path = implode('/', array_slice(explode('/', dirname($registrationFile)), -2));
+            if (\in_array($path, $excludeList, true)) {
+                continue;
+            }
+            $finalRegistrationFiles[] = $registrationFile;
+
+        }
+        return $finalRegistrationFiles;
     }
 }
